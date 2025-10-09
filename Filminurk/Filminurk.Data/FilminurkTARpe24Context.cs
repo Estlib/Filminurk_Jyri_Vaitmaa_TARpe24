1126bb3108eb7efc491763f3fe89c26eaf3d8347
Estlib <75094683+Estlib@users.noreply.github.com>
2025-10-09 11:05:30 +0300

    Movies modeldata for both domain and viewmodels

---
+++ Filminurk/Filminurk.Data/FilminurkTARpe24Context.cs
@@ -0,0 +1,15 @@
+﻿using Filminurk.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Data
+{
+    public class FilminurkTARpe24Context : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ Filminurk/Filminurk/Models/Movies/MoviesIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Filminurk.Core.Domain;
+
+namespace Filminurk.Models.Movies
+{
+    public class MoviesIndexViewModel
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public DateOnly FirstPublished { get; set; }
+        public decimal? CurrentRating { get; set; }
+        //public List<UserComment>? Reviews { get; set; }
+
+        /* 3 õpilase valitud andmetüüpi */
+        public string? CountryOfOrigin { get; set; }
+        public Genre? MovieGenre { get; set; }
+    }
+}
